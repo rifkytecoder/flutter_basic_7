5fa3695fd036aa9fd8ff3243d03d4e95d5135da9
rifkytecoder <rifkytecoder@gmail.com>
2022-11-12 11:59:07 +0800

    learn GetX state management Reactive state and Simple state

---
+++ lib/controller/5_home_getx_controller.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+
+class HomeControllerx extends GetxController {
+  // todo variable ini biar bisa selalu dipantau setiap kali ada perubahan
+
+  var dataPantauReactive = 0.obs; // untuk reactive state manager
+
+  int dataPantauSimple = 0; // untuk simple state manager
+
+  void tambahData() {
+    dataPantauReactive += 1;
+    // dataPantauSimple += 1;
+  }
+
+  // todo fungsi tambah data kelipatan 5
+  void tambahData5() {
+    dataPantauSimple += 1;
+    if (dataPantauSimple % 5 == 0) {
+      update();
+    }
+  }
+
+  // todo fungsi merefresh tampilan agar nilainya terupdate ini berlaku hanya di Simple State manager
+  void refreshTampilan() {
+    update();
+  }
+}
--- lib/section_7_getx/5_getx_reactive_n_simple.dart
+++ lib/section_7_getx/5_getx_reactive_n_simple.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'controller/5_home_getx_controller.dart';
+import '../controller/5_home_getx_controller.dart';
 
 void main() => runApp(const MyApp());
 
